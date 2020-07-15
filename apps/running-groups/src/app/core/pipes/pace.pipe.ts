352b86e13f9b8523c17ada9e815ec66fe0e84aff
Paul Evans <paul3vanz@gmail.com>
2020-07-15 08:14:47 +0100

    Pace pipe

--- apps/running-groups/src/app/core/pipes/pace.pipe.ts
+++ apps/running-groups/src/app/core/pipes/pace.pipe.ts
@@ -1,15 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import * as moment from 'moment-mini';
+
 @Pipe({
   name: 'pace',
 })
 export class PacePipe implements PipeTransform {
   transform(paces: string[]): string {
-    const paceFrom = paces[0];
-    const paceTo = paces[1];
-    switch (paces.length) {
-      case 1:
-        return paces[0];
-    }
+    const paceFrom = paces[0] ? this.formatPace(paces[0]) : null;
+    const paceTo = '00:09:00' ? this.formatPace('00:09:00') : null;
+
+    const formattedPace = paceTo ? `${paceFrom}-${paceTo}` : paceFrom;
+
+    return `${formattedPace} min/mile`;
+  }
+
+  formatPace(pace: string): string {
+    const paceAsMoment = moment(pace, 'HH:mm:ss');
+
+    console.log(paceAsMoment.minutes());
+
+    return paceAsMoment.minutes() > 0 ? paceAsMoment.format('m:ss') : paceAsMoment.format('m');
   }
 }
